7a2f7d4a79883603bddd29a3f4140d13a834775b
Rahul N <rahulnagarajrahul1234@gmail.com>
2026-01-17 23:45:34 +0530

    Configure dynamic port for Railway

--- src/main/java/com/ecommerce/user_service/controller/UserQueryController.java
+++ src/main/java/com/ecommerce/user_service/controller/UserQueryController.java
@@ -3,10 +3,10 @@ package com.ecommerce.user_service.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("user")
 public class UserQueryController {
 
-    @GetMapping
+    @GetMapping("/test")
     public String test(){
         return "I am from user service";
     }
