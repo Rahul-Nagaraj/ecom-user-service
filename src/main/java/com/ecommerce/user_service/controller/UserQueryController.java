193db695e9179a46b31d96709e3d90b7c148ad60
Rahul N <rahulnagarajrahul1234@gmail.com>
2026-01-18 11:20:40 +0530

    Corrected api path for routing

--- src/main/java/com/ecommerce/user_service/controller/UserQueryController.java
+++ src/main/java/com/ecommerce/user_service/controller/UserQueryController.java
@@ -1,9 +1,11 @@
 package com.ecommerce.user_service.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("user")
+@RestController
+@RequestMapping("api/user")
 public class UserQueryController {
 
     @GetMapping("/test")
